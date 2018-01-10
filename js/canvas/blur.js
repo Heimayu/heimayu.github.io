96f55a1c26068987eb42ff10c7cd159092a3f594
ligao4 <ligao4@iflytek.com>
2018-01-10 09:38:36 +0800

    Site updated: 2018-01-10 09:38:32

---
+++ js/canvas/blur.js
@@ -0,0 +1,18 @@
+/**
+ * 玩转照片-初识canvas(三)
+ * @author heimayu
+ * @time   2018-01-02
+ */
+
+var canvas3 = {
+  
+  //初始化
+  init: function() {
+    var that = this;
+    
+  }
+}
+
+window.onload = function() {
+  canvas3.init();
+};
